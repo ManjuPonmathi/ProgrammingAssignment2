4e21343fabc99adb2b3c768f5bad25b588bb5e32
Manju <manju_gw@yahoo.co.in>
2015-07-24 17:25:54 +0530

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,9 @@
 ## Put comments here that give an overall description of what your
 ## The first function, makeCacheMatrix creates a special "matrix", which is really a list containing a function to
-set the value of the matrix
-get the value of the matrix
-set the value of the matrix
-get the value of the matrix
+##set the value of the matrix
+##get the value of the matrix
+##set the value of the matrix
+##get the value of the matrix
 
 
 ## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
